0ef727b4700ae25bede1475e017c19dabce094d5
dhananjay1438 <dhananjayapanage11@gmail.com>
2019-05-13 01:50:58 +0530

    created lists with range and used it

---
+++ Basic/loops/forLoops/usingListsCreatedWithRange.py
@@ -0,0 +1,20 @@
+oneToHundred = list(range(1, 101))
+
+evenNumbers = list(range(100, 1001, 2))
+
+oddNumbers = list(range (101, 1001, 2))
+
+hundredToOne = list(range(100, 0, -1))
+
+print(oneToHundred, end=" ")
+print()
+
+print(hundredToOne, end=" ")
+print()
+
+print(evenNumbers, end=" ")
+print()
+
+print(oddNumbers, end=" ")
+print()
+
