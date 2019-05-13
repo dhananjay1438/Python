64c89cca4c65fe15142fcdaa412ff28f696fe8f7
dhananjay1438 <dhananjayapanage11@gmail.com>
2019-05-13 12:04:01 +0530

    Strings are immutable

---
+++ lists/areStringsImmutable.py
@@ -0,0 +1,10 @@
+string = "Hello"
+
+string = string.replace('o', 'a')
+
+# String values cannot be changed with indices it must use special function called 'replace()'
+print(string)
+
+
+# 'replace()' must be used only with string object otherwise it won't work
+someStr = "World"
